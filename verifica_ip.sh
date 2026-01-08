27c37e4108808de540b537b4bc0fd45114d56e3d
Anleglift <victorivanus22@gmail.com>
2026-01-08 00:55:22 +0000

    Am schimbat arhitectura script-ului pentru a folosi o functie care primeste 3 parametri.

--- verifica_ip.sh
+++ verifica_ip.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-cat /etc/hosts | while read -r ip name rest; do
-    real_ip=$(nslookup "$name" 1.1.1.1 2>/dev/null | awk '/^Address: /{c++; if(c==1){print $2; exit}}')
-    echo "real ip = $real_ip and ip = $ip"
+check_host_ip() {
+    local name="$1"
+    local ip="$2"
+    local dns="$3"
+
+    real_ip=$(nslookup "$name" "$dns" 2>/dev/null | awk '/^Address: /{c++; if(c==1){print $2; exit}}')
     if [ -n "$real_ip" ] && [ "$real_ip" != "$ip" ]; then
         echo "Wrong IP for $name in /etc/hosts!"
     fi
-done
+}
+
+cat /etc/hosts | while read -r ip name rest; do
+    check_host_ip "$name" "$ip" "1.1.1.1"
+done 
