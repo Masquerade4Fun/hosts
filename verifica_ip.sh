04387428b48e572daf63654c66f61ad6982524e5
Masquerade4Fun <96654795+Masquerade4Fun@users.noreply.github.com>
2026-01-08 17:17:54 +0200

    Update verifica_ip.sh

--- verifica_ip.sh
+++ verifica_ip.sh
@@ -4,7 +4,7 @@ check_host_ip() {
     local name="$1"
     local ip="$2"
     local dns="$3"
-
+    ##Masuqerade se uita la un smek
     real_ip=$(nslookup "$name" "$dns" 2>/dev/null | awk '/^Address: /{c++; if(c==1){print $2; exit}}')
     if [ -n "$real_ip" ] && [ "$real_ip" != "$ip" ]; then
         echo "Wrong IP for $name in /etc/hosts!"
